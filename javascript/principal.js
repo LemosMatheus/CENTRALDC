e5ed7cd2fd1ccf0b7621a074861623c7e79e51d7
m4r14n4 <mariana.paz@escolar.ifrn.edu.br>
2025-07-24 11:48:16 -0300

    cards

--- javascript/principal.js
+++ javascript/principal.js
@@ -1,2 +1,18 @@
 let nome = localStorage.getItem("armazem")
 document.getElementById("titulo").innerText = `Olá, ${nome}`
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btngotham = document.getElementById("btngotham");
+
+    btngotham.addEventListener('click', () => {
+        document.getElementById("corpogotham").classList.toggle("oculto")
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btngotham = document.getElementById("btnmetropolis");
+
+    btngotham.addEventListener('click', () => {
+        document.getElementById("corpometropolis").classList.toggle("oculto")
+    })
+})
